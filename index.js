e82ea4965adeb07e198a13e0191459fcb1501e9f
Worawut Boontan <worawut@theionweb.com>
2025-09-10 17:09:06 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+export async function generateSha256Checksum(jsonPayload) {
+  const comparePayload =
+    typeof jsonPayload === "string" ? jsonPayload : JSON.stringify(jsonPayload);
+  const payload =
+    filename?.length > 0
+      ? filename.join("|") + "|" + comparePayload
+      : comparePayload;
+  const encoder = new TextEncoder();
+  const dataAsUint8Array = encoder.encode(payload);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", dataAsUint8Array);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+  return hashHex;
+}
